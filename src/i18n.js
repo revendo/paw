ca9a29b07101697a84da257baf5d09af39d0451b
Jakob Baranowski <jakob@baranowski.cc>
2023-05-11 11:30:03 +0200

    Finished vite migration, updated version number

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,8 @@ import {createI18n} from 'vue-i18n'
 
 export default createI18n({
     legacy: false,
-    locale: import.meta.env.I18N_LOCALE || 'en',
-    fallbackLocale: import.meta.env.I18N_FALLBACK_LOCALE || 'en',
+    locale: import.meta.env.VITE_I18N_LOCALE || 'en',
+    fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || 'en',
     globalInjection: true,
     silentFallbackWarn: true,
 })
