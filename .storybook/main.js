ff41d264bb5dba2f11c0213536c8d110f66087e2
DavidFelbarRevendo <david.felbar@revendo.ch>
2024-03-20 14:04:53 +0100

    Storybook addons removed post update, dependencies updated

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     stories: [
         "../src/**/*.stories.mdx",
         "../src/**/*.stories.@(js|jsx|ts|tsx)",
@@ -9,30 +9,8 @@ module.exports = {
         "@storybook/addon-essentials",
         "@storybook/addon-docs",
         "@storybook/addon-interactions",
-        {
-            name: "@storybook/addon-styling",
-            options: {
-                // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-                // For more details on this addon's options.
-                postCss: true,
-            },
-        },
-        {
-            name: "@storybook/addon-postcss",
-            options: {
-                cssLoaderOptions: {
-                    // When you have split your css over multiple files
-                    // and use @import('./other-styles.css')
-                    importLoaders: 1,
-                },
-                postcssLoaderOptions: {
-                    // When using postCSS 8
-                    implementation: require("postcss"),
-                },
-            },
-        },
+        "@storybook/addon-styling",
         "storybook-tailwind-dark-mode",
-        "@storybook/addon-mdx-gfm",
     ],
     staticDirs: ["../src/assets"],
     framework: {
--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 // Auto-generated file by create-index-files.js. Do not edit manually
-import PawButton from "./src/components/Button/Button.vue";
-import PawCheckbox from "./src/components/Checkbox/Checkbox.vue";
-import PawChip from "./src/components/Chip/Chip.vue";
-import PawCrazyInput from "./src/components/CrazyInput/CrazyInput.vue";
-import PawDatePicker from "./src/components/Datepicker/DatePicker.vue";
-import PawDropDown from "./src/components/Dropdown/DropDown.vue";
-import PawFloatingBar from "./src/components/FloatingBar/FloatingBar.vue";
-import PawIcon from "./src/components/Icon/Icon.vue";
-import PawLabel from "./src/components/Label/Label.vue";
-import PawLazyField from "./src/components/LazyField/LazyField.vue";
-import PawLink from "./src/components/Link/Link.vue";
-import PawList from "./src/components/NavigationList/List.vue";
-import PawListItem from "./src/components/NavigationList/ListItem.vue";
-import PawRadio from "./src/components/Radio/Radio.vue";
-import PawSwitch from "./src/components/Switch/Switch.vue";
+import PawTypography from "./src/components/Typography/Typography.vue";
 import PawTable from "./src/components/Table/Table.vue";
+import PawSwitch from "./src/components/Switch/Switch.vue";
+import PawRadio from "./src/components/Radio/Radio.vue";
+import PawListItem from "./src/components/NavigationList/ListItem.vue";
+import PawList from "./src/components/NavigationList/List.vue";
+import PawLink from "./src/components/Link/Link.vue";
+import PawLazyField from "./src/components/LazyField/LazyField.vue";
+import PawLabel from "./src/components/Label/Label.vue";
+import PawIcon from "./src/components/Icon/Icon.vue";
+import PawFloatingBar from "./src/components/FloatingBar/FloatingBar.vue";
+import PawDropDown from "./src/components/Dropdown/DropDown.vue";
+import PawDatePicker from "./src/components/Datepicker/DatePicker.vue";
+import PawCrazyInput from "./src/components/CrazyInput/CrazyInput.vue";
+import PawColorSwatches from "./src/components/ColorSwatches/ColorSwatches.vue";
+import PawChip from "./src/components/Chip/Chip.vue";
+import PawCheckbox from "./src/components/Checkbox/Checkbox.vue";
+import PawButton from "./src/components/Button/Button.vue";
 
 export {
-    PawButton,
-    PawCheckbox,
-    PawChip,
-    PawCrazyInput,
-    PawDatePicker,
-    PawDropDown,
-    PawFloatingBar,
-    PawIcon,
-    PawLabel,
-    PawLazyField,
-    PawLink,
-    PawList,
-    PawListItem,
-    PawRadio,
+    PawTypography,
+    PawTable,
     PawSwitch,
-    PawTable
+    PawRadio,
+    PawListItem,
+    PawList,
+    PawLink,
+    PawLazyField,
+    PawLabel,
+    PawIcon,
+    PawFloatingBar,
+    PawDropDown,
+    PawDatePicker,
+    PawCrazyInput,
+    PawColorSwatches,
+    PawChip,
+    PawCheckbox,
+    PawButton
 };
