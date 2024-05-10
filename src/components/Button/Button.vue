d0e2096bfb461c7fcd5a3ecf6a295fefee4bad9c
DavidFelbarRevendo <david.felbar@revendo.ch>
2024-05-10 14:07:09 +0200

    PawButton - Processing state transitions corrected for better UX

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -24,13 +24,14 @@
       >{{ !processing ? icon : 'sync' }}</PawIcon
     >    
     <transition
-      enter-active-class="transition ease-in-out "
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
+      enter-active-class="transitiona ease-in"
+      enter-from-class="opacity-0 delay-150 ease-out"
+      enter-to-class="opacity-100 "
     >
       <PawLoadingSpinner 
         v-show="processing" 
-        class="mx-2 my-auto" 
+        class="transition w-full delay-150"
+        :class="processing ? 'max-w-fit my-auto py-0.5 mx-2' : 'max-w-0 mx-0'" 
         loaderSize="sm" 
         :spinnerColorClasses="setClassNames().includes('-action-') ? 'fill-white text-gray-300 dark:text-gray-600': 'fill-action-500 text-gray-300 dark:text-gray-600'"
       />
