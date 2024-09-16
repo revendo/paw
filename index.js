10c43f8d6899b80504748a85bd96a1def52980ae
Jakob Baranowski <jakob@baranowski.cc>
2024-09-16 15:33:34 +0200

    :zap: Version increase and build

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // Auto-generated file by create-index-files.js. Do not edit manually
-import PawTypography from "./src/components/Typography/Typography.vue";
-import PawTable from "./src/components/Table/Table.vue";
-import PawSwitch from "./src/components/Switch/Switch.vue";
-import PawRadio from "./src/components/Radio/Radio.vue";
-import PawListItem from "./src/components/NavigationList/ListItem.vue";
-import PawList from "./src/components/NavigationList/List.vue";
-import PawLoadingSpinner from "./src/components/LoadingSpinner/LoadingSpinner.vue";
-import PawLink from "./src/components/Link/Link.vue";
-import PawLazyField from "./src/components/LazyField/LazyField.vue";
-import PawLabel from "./src/components/Label/Label.vue";
-import PawIcon from "./src/components/Icon/Icon.vue";
-import PawFloatingBar from "./src/components/FloatingBar/FloatingBar.vue";
-import PawDropDown from "./src/components/Dropdown/DropDown.vue";
-import PawDatePicker from "./src/components/Datepicker/DatePicker.vue";
-import PawCrazyInput from "./src/components/CrazyInput/CrazyInput.vue";
-import PawColorSwatches from "./src/components/ColorSwatches/ColorSwatches.vue";
-import PawChip from "./src/components/Chip/Chip.vue";
-import PawCheckbox from "./src/components/Checkbox/Checkbox.vue";
 import PawButton from "./src/components/Button/Button.vue";
+import PawCheckbox from "./src/components/Checkbox/Checkbox.vue";
+import PawChip from "./src/components/Chip/Chip.vue";
+import PawColorSwatches from "./src/components/ColorSwatches/ColorSwatches.vue";
+import PawCrazyInput from "./src/components/CrazyInput/CrazyInput.vue";
+import PawDatePicker from "./src/components/Datepicker/DatePicker.vue";
+import PawDropDown from "./src/components/Dropdown/DropDown.vue";
+import PawFloatingBar from "./src/components/FloatingBar/FloatingBar.vue";
+import PawIcon from "./src/components/Icon/Icon.vue";
+import PawLabel from "./src/components/Label/Label.vue";
+import PawLazyField from "./src/components/LazyField/LazyField.vue";
+import PawLink from "./src/components/Link/Link.vue";
+import PawLoadingSpinner from "./src/components/LoadingSpinner/LoadingSpinner.vue";
+import PawList from "./src/components/NavigationList/List.vue";
+import PawListItem from "./src/components/NavigationList/ListItem.vue";
+import PawRadio from "./src/components/Radio/Radio.vue";
+import PawSwitch from "./src/components/Switch/Switch.vue";
+import PawTable from "./src/components/Table/Table.vue";
+import PawTypography from "./src/components/Typography/Typography.vue";
 
 export {
-    PawTypography,
-    PawTable,
-    PawSwitch,
-    PawRadio,
-    PawListItem,
-    PawList,
-    PawLoadingSpinner,
-    PawLink,
-    PawLazyField,
-    PawLabel,
-    PawIcon,
-    PawFloatingBar,
-    PawDropDown,
-    PawDatePicker,
-    PawCrazyInput,
-    PawColorSwatches,
-    PawChip,
+    PawButton,
     PawCheckbox,
-    PawButton
+    PawChip,
+    PawColorSwatches,
+    PawCrazyInput,
+    PawDatePicker,
+    PawDropDown,
+    PawFloatingBar,
+    PawIcon,
+    PawLabel,
+    PawLazyField,
+    PawLink,
+    PawLoadingSpinner,
+    PawList,
+    PawListItem,
+    PawRadio,
+    PawSwitch,
+    PawTable,
+    PawTypography
 };
